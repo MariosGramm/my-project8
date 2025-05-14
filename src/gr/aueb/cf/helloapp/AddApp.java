80bf00ee4e54bc0b1e8eb72fa5a0a37e185eb1a7
MariosGramm <mariosgramm21@gmail.com>
2025-05-14 20:14:06 +0300

    Add AddApp

--- src/gr/aueb/cf/helloapp/AddApp.java
+++ src/gr/aueb/cf/helloapp/AddApp.java
@@ -1,4 +1,11 @@
 package gr.aueb.cf.helloapp;
 
 public class AddApp {
+
+    public static void main(String[] args) {
+        int a = 1 ;
+        int b = 2 ;
+
+        int sum = a + b ;
+    }
 }
